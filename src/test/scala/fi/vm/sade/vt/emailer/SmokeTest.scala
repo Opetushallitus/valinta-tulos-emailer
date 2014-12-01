29ceb1d9d5666134e9d6f051d7b9534b7195a841
Juha Paananen <juha.paananen@gmail.com>
2014-12-01 11:08:31 +0200

    start registry later to reveal possible errors

--- src/test/scala/fi/vm/sade/vt/emailer/SmokeTest.scala
+++ src/test/scala/fi/vm/sade/vt/emailer/SmokeTest.scala
@@ -8,13 +8,13 @@ import org.specs2.mutable._
 
 
 class SmokeTest extends Specification with HttpComponentsClient with Logging {
-  val registry: Registry = Registry.fromString(Option(System.getProperty("valintatulos.profile")).getOrElse("it"))
-  registry.start
+  lazy val registry: Registry = Registry.fromString(Option(System.getProperty("valintatulos.profile")).getOrElse("it"))
 
   override def baseUrl: String = "http://localhost:" + ValintatulosServiceRunner.valintatulosPort + "/valinta-tulos-service"
 
   "Generate test fixtures and retrieve them" should {
     "PUT /util/fixtures/generate" in {
+      registry.start
       put("util/fixtures/generate?hakemuksia=3&hakukohteita=2") {
         !registry.mailer.sendBatch().isEmpty
         registry.asInstanceOf[IT].lastEmailSize() equals(3)
