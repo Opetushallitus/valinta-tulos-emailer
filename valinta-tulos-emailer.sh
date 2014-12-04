d9def1df65cefbc8b00a4b69f70f8d6d1575df03
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-12-04 15:07:01 +0200

    Enhance starting script

--- valinta-tulos-emailer.sh
+++ valinta-tulos-emailer.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-java -jar *.jar
+pidfile=/var/run/valinta-tulos-emailer.pid
+logfile=/var/log/valinta-tulos-emailer.log
+
+if [ -e ${pidfile} ]; then
+    pid=`cat ${pidfile}`
+    rm ${pidfile}
+fi
+
+if [ -z "$pid" ] || ! ps -p ${pid} >&- ; then
+    nohup java -server -jar *.jar &> ${logfile} &
+    echo $! > ${pidfile}
+else
+    echo "valinta-tulos-emailer was already running with pid: ${pid}"
+fi
