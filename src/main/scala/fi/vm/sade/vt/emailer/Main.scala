1c706811986991960ce1bfbfa7454f13dc92984a
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-12-04 15:06:45 +0200

    Add start / stop messages

--- src/main/scala/fi/vm/sade/vt/emailer/Main.scala
+++ src/main/scala/fi/vm/sade/vt/emailer/Main.scala
@@ -2,17 +2,21 @@ package fi.vm.sade.vt.emailer
 
 import fi.vm.sade.vt.emailer.config.Registry
 import fi.vm.sade.vt.emailer.config.Registry.Registry
-import fi.vm.sade.vt.emailer.util.Timer
+import fi.vm.sade.vt.emailer.util.{Logging, Timer}
 
-object Main extends App {
+object Main extends App with Logging {
+  logger.info("***** VT-emailer started *****")
   val registry: Registry = Registry.fromString(Option(System.getProperty("vtemailer.profile")).getOrElse("default"))
   registry.start
   Timer.timed("Batch send") {
    val ids =  registry.mailer.sendMail
     if (ids.nonEmpty) {
+      logger.info(s"Job sent succesfully, jobId: $ids")
       println(s"Job sent succesfully, jobId: $ids")
     } else {
       println("Job failed. More info in logs.")
     }
   }
+  logger.info("***** VT-emailer finished *****")
+
 }
