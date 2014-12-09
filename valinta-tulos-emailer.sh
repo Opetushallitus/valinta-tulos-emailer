e5b29a4b53564a21c7d9db130b22ebd8e0f75c9d
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-12-09 11:37:09 +0200

    fix directories

--- valinta-tulos-emailer.sh
+++ valinta-tulos-emailer.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-pidfile=/var/run/valinta-tulos-emailer.pid
-logfile=/var/log/valinta-tulos-emailer.log
+JAVA_HOME=/data00/oph/java/jdk1.7.0_45/jre
+PATH=$PATH:$JAVA_HOME/bin
+
+user_home=/data00/oph/valinta-tulos-emailer
+pidfile=${user_home}/valinta-tulos-emailer.pid
+logfile=${user_home}/logs/valinta-tulos-runner.log
 
 if [ -e ${pidfile} ]; then
     pid=`cat ${pidfile}`
@@ -10,7 +14,7 @@ if [ -z "$pid" ] || ! ps -p ${pid} >&- ; then
     if [ -e ${pidfile} ]; then
         rm ${pidfile}
     fi
-    nohup java -server -jar *.jar &> ${logfile} &
+    nohup java -Duser.home=${user_home}-server -jar *.jar &> ${logfile} &
     echo $! > ${pidfile}
 else
     echo "valinta-tulos-emailer was already running with pid: ${pid}"
