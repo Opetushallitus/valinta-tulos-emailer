19d3ed5b02e5eae549562ba480173e9fcdc515a4
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-12-09 14:22:10 +0200

    Add class path to run script

--- valinta-tulos-emailer.sh
+++ valinta-tulos-emailer.sh
@@ -2,6 +2,7 @@
 JAVA_HOME=/data00/oph/java/jdk1.7.0_45/jre
 PATH=$PATH:$JAVA_HOME/bin
 
+classpath="oph-configuration"
 user_home=/data00/oph/valinta-tulos-emailer
 pidfile=${user_home}/valinta-tulos-emailer.pid
 logfile=${user_home}/logs/valinta-tulos-runner.log
@@ -14,7 +15,7 @@ if [ -z "$pid" ] || ! ps -p ${pid} >&- ; then
     if [ -e ${pidfile} ]; then
         rm ${pidfile}
     fi
-    nohup java -Duser.home=${user_home} -server -jar *.jar &> ${logfile} &
+    nohup java -Duser.home=${user_home} -cp ${classpath} -server -jar *.jar &> ${logfile} &
     echo $! > ${pidfile}
 else
     echo "valinta-tulos-emailer was already running with pid: ${pid}"
