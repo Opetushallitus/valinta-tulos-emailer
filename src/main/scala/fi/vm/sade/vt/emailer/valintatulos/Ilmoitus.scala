0c2cb74beb5a58524d31801508a46a8bfdb50751
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-07-14 11:27:27 +0300

    new API added

---
+++ src/main/scala/fi/vm/sade/vt/emailer/valintatulos/Ilmoitus.scala
@@ -0,0 +1,43 @@
+package fi.vm.sade.vt.emailer.valintatulos
+
+
+import java.util.Date
+
+import fi.vm.sade.vt.emailer.valintatulos.LahetysSyy.LahetysSyy
+
+case class Ilmoitus(
+                     hakemusOid: String,
+                     hakijaOid: String,
+                     asiointikieli: String,
+                     etunimi: String,
+                     email: String,
+                     deadline: Option[Date],
+                     hakukohteet: List[Hakukohde],
+                     haku: Haku
+                   )
+
+case class Hakukohde(
+                      oid: String,
+                      lahetysSyy: LahetysSyy,
+                      ehdollisestiHyvaksyttavissa: Boolean,
+                      hakukohteenNimet: Map[String, String],
+                      tarjoajaNimet: Map[String, String]
+                    )
+
+case class Haku(
+                 oid: String,
+                 nimi: Map[String, String]
+               )
+
+case class LahetysKuittaus(
+                            hakemusOid: String,
+                            hakukohteet: List[String],
+                            mediat: List[String]
+                          )
+
+object LahetysSyy {
+  type LahetysSyy = String
+  val vastaanottoilmoitus: LahetysSyy = "VASTAANOTTOILMOITUS"
+  val ehdollisen_periytymisen_ilmoitus: LahetysSyy = "EHDOLLISEN_PERIYTYMISEN_ILMOITUS"
+  val sitovan_vastaanoton_ilmoitus: LahetysSyy = "SITOVAN_VASTAANOTON_ILMOITUS"
+}
