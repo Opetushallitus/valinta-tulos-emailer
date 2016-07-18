75369c50af1ecc6066b27779e6ef22d58816e7de
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2016-07-18 16:06:22 +0300

    Added hakukohde replacement to email

--- src/main/scala/fi/vm/sade/vt/emailer/ryhmasahkoposti/GroupEmail.scala
+++ src/main/scala/fi/vm/sade/vt/emailer/ryhmasahkoposti/GroupEmail.scala
@@ -2,6 +2,7 @@ package fi.vm.sade.vt.emailer.ryhmasahkoposti
 
 import fi.vm.sade.groupemailer.{Recipient, Replacement}
 import fi.vm.sade.vt.emailer.valintatulos
+import fi.vm.sade.vt.emailer.valintatulos.LahetysSyy._
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
@@ -23,6 +24,8 @@ object VTEmailerReplacement {
 
   def hakukohteet(hakukohteet: List[Hakukohde]) = Replacement("hakukohteet", hakukohteet)
 
+  def hakukohde(hakukohde: Hakukohde) = Replacement("hakukohde", hakukohde)
+
   private def deadlineText(date: Option[DateTime]): String = date match {
     case Some(deadline) => fmt.print(deadline)
     case _ => ""
@@ -43,14 +46,30 @@ object VTRecipient {
       translations.get(language.toLowerCase).orElse(translations.get("fi")).getOrElse(translations.head._2).get
     }
 
+    def getHakukohtees: Replacement = {
+      val hakukohteet = valintatulosRecipient.hakukohteet
+      val lahetysSyy: LahetysSyy = hakukohteet.head.lahetysSyy
+      if (hakukohteet.size == 1 && (lahetysSyy.equals(ehdollisen_periytymisen_ilmoitus) || lahetysSyy.equals(sitovan_vastaanoton_ilmoitus))) {
+        val hakukohde = hakukohteet.head
+        VTEmailerReplacement.hakukohde(Hakukohde(hakukohde.oid, getTranslation(hakukohde.hakukohteenNimet),
+          getTranslation(hakukohde.tarjoajaNimet), hakukohde.ehdollisestiHyvaksyttavissa)
+        )
+      } else if (lahetysSyy.equals(vastaanottoilmoitus)) {
+        VTEmailerReplacement.hakukohteet(hakukohteet.map(hakukohde =>
+          Hakukohde(hakukohde.oid, getTranslation(hakukohde.hakukohteenNimet),
+            getTranslation(hakukohde.tarjoajaNimet), hakukohde.ehdollisestiHyvaksyttavissa)
+        ))
+      } else {
+        throw new IllegalArgumentException("Failed to add hakukohde information to recipient. Hakemus " + valintatulosRecipient.hakemusOid +
+          ". LahetysSyy was " + hakukohteet.head.lahetysSyy + " and there was " + hakukohteet.size + "hakukohtees")
+      }
+    }
+
     val replacements = List(
       VTEmailerReplacement.firstName(valintatulosRecipient.etunimi),
       VTEmailerReplacement.deadline(valintatulosRecipient.deadline),
       VTEmailerReplacement.haunNimi(getTranslation(valintatulosRecipient.haku.nimi)),
-      VTEmailerReplacement.hakukohteet(valintatulosRecipient.hakukohteet.map(hakukohde =>
-        Hakukohde(hakukohde.oid, getTranslation(hakukohde.hakukohteenNimet),
-          getTranslation(hakukohde.tarjoajaNimet), hakukohde.ehdollisestiHyvaksyttavissa)
-      ))
+      getHakukohtees
     )
     Recipient(Some(valintatulosRecipient.hakijaOid), valintatulosRecipient.email, valintatulosRecipient.asiointikieli, replacements)
   }
